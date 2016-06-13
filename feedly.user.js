662b318806032446d4a9a30f2e64d0c769b169dd
Alexander Q <qalexx@gmail.com>
2016-06-13 22:20:37 +0300

    feedly script updated

--- feedly.user.js
+++ feedly.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Shorpy large images at Feedly
 // @namespace  q.alexander.feedly.shorpy
-// @version    0.4
+// @version    0.5
 // @description  Replace small Shorpy.com previews with large images in Feedly
 // @match      http://feedly.com/*
 // @match      https://feedly.com/*
@@ -19,18 +19,18 @@
     '.condensed .entryholder .u100Entry {margin-left: 20px !important; margin-right: 0 !important; max-width: none !important;}' +
       '.content img[style] {max-height: 650px !important; height: auto !important; max-width: 100% !important; width: auto !important; }' +
     '.entryBody a {font-weight: normal !important;}' +
-    'div .button.primary.small {background-color: #aaa !important;}' +
-    'div .entryBody {max-width: 100% !important;}'
+    'div .fx-button.primary.small {background-color: #aaa !important;}' +
+    'div .entryBody {max-width: 100% !important;}';
   head.appendChild(style);
   console.log('styles added');
 })();
 
 document.addEventListener("DOMNodeInserted", function (ev) {
-	el = document.querySelector('img[src^="http://www.shorpy.com/files/images/"]');
-    if(!el) {return}
-	var s = el.getAttribute('src');
+	el = document.querySelector('img[data-original^="http://www.shorpy.com/files/images/"]');
+    if(!el) {return;}
+	var s = el.getAttribute('data-original');
     var n = s.replace('.preview.jpg', '.jpg');
-    if(s == n) {return}
+    if(s == n) {return;}
     console.log("Replacing image url " + s + " with " + n);
 	el.setAttribute('src', n);
 }, false);
