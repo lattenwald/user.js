66c6dcbc82027cc4376d5dfe5b889993d0ee5025
Alexander Q <qalexx@gmail.com>
2018-10-30 15:56:30 +0300

    video should be transparentish too

--- lepra.user.js
+++ lepra.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Lepra
 // @namespace  q.alexander.feedly.shorpy
-// @version    0.4
+// @version    0.4.1
 // @description  Miscellaneous improvements
 // @match      http://leprosorium.ru/*
 // @match      http://*.leprosorium.ru/*
@@ -20,7 +20,7 @@
     style.type = 'text/css';
     console.log('styles');
     style.innerHTML =
-        '.post img, .c_body img {opacity: 0.1;} .post:hover img, .c_body:hover img {opacity: 1;} div.l-content { background-image: none; }';
+        '.post img, .c_body img, .post video {opacity: 0.1;} .post:hover img, .c_body:hover img, .post:hover video {opacity: 1;} div.l-content { background-image: none; }';
     head.appendChild(style);
     console.log('styles added');
 })();
