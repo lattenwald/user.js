d18dd33e9a455ffe735cb92e61e312a120d23084
Alexander Q <qalexx@gmail.com>
2019-01-23 17:14:37 +0300

    nintendolife: navigation focuses on title image for each game

--- nintendolife.user.js
+++ nintendolife.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Apps list navigation for NintendoLife
 // @namespace    q.alexander.nintendolife.navigation
-// @version      0.1.0
+// @version      0.1.1
 // @description  enter something useful
 // @author       You
 // @match        https://www.nintendolife.com/*
@@ -10,7 +10,7 @@
 
 //window.addEventListener('load', function() {
 (function(){
-    var h2s = jQuery('section.text>h2');
+    var h2s = jQuery('section.text>h2').prev();
     var idx = -1;
     jQuery(document).keypress(function(e){
         var doit=false;
@@ -32,7 +32,7 @@
             console.log('scrolling to ' + idx);
             jQuery('html, body').animate({
                 scrollTop: jQuery(h2s[idx]).offset().top
-	        }, 500);
+            }, 500);
         }
     });
 })();
