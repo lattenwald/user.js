b8c4e80504e85a9bb3f00511d67b069e0086ff63
Alexander Q <qalexx@gmail.com>
2019-02-20 12:34:50 +0300

    inoreader shorpy fixes

--- ino.user.js
+++ ino.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Style modifications for inoreader.com with support for bigger images from shorpy.com
 // @namespace  q.alexander.inoreader
-// @version    0.7.5
+// @version    0.7.6
 // @description  Minor style changes
 // @match      http://www.inoreader.com/*
 // @match      https://www.inoreader.com/*
@@ -12,7 +12,9 @@
 (function() {
   var head, style;
   head = document.getElementsByTagName('head')[0];
-  if (!head) { return; }
+  if (!head) {
+    return;
+  }
   style = document.createElement('style');
   style.type = 'text/css';
   style.innerHTML =
@@ -21,35 +23,29 @@
   console.log('styles added');
 })();
 
-document.addEventListener("DOMNodeInserted", function (e) {
+document.addEventListener("DOMNodeInserted", function(e) {
   // Uploadvr icons -> images
   $(e.relatedNode)
     .find('img[src^="https://uploadvr.com/wp-content/uploads/"]')
     .each(function(idx, el) {
       var s = el.src;
       var n = s.replace(/-\d+x\d+.([^\.]+)$/, '.$1');
-      if (s == n) {return;}
+      if (s == n) {
+        return;
+      }
       console.log("Replacing image url " + s + " with " + n);
       el.src = n;
     });
 
   $(e.relatedNode)
-    .find('img[src*="http://www.shorpy.com/files/images/"]')
+    .find('img[data-original-src*="https://www.shorpy.com/files/images/"]')
     .each(function(idx, el) {
-      var s = el.src;
+      var s = el.getAttribute('data-original-src');
       var n = s.replace(/^.*(www\.shorpy\.com.*)\.preview(\.\w+).*$/, "https://$1$2");
-      if (s == n) {return;}
+      if (el.src == n) {
+        return;
+      }
       console.log("Replacing image url " + s + " with " + n);
-      el.src = n;
-    });
-
-  $(e.relatedNode)
-    .find('img[alt^="SHORPY-"]')
-    .each(function(idx, el) {
-	  var s = el.alt;
-	  var n = "https://www.shorpy.com/files/images/" + s.replace("\.preview", "");
-      if (el.src == n) {return;}
-      console.log("Replacing image url " + el.src + " with " + n);
       el.src = n;
     });
 }, false);
