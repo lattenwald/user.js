eeb0770fe766984b9441918c9b21cd26a83a6933
Alexander Q <qalexx@gmail.com>
2019-07-25 15:44:53 +0300

    nintendolife:supporting pagination on multiple pages

--- nintendolife.user.js
+++ nintendolife.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Apps list navigation for NintendoLife
 // @namespace    q.alexander.nintendolife.navigation
-// @version      0.1.2
+// @version      0.2.0
 // @description  enter something useful
 // @author       You
 // @match        https://www.nintendolife.com/*
@@ -10,29 +10,48 @@
 
 //window.addEventListener('load', function() {
 (function(){
-    var h2s = jQuery('div.list>div.list-item').prev();
+    var items = jQuery('div.list>div.list-item');
     var idx = -1;
+    var pageNum = Number(jQuery('.pages .active').text());
+    if (!pageNum)
+        pageNum=1;
+
     jQuery(document).keypress(function(e){
         var doit=false;
         var url=false;
         if (e.which == 106) { //next
             idx++;
-            if (idx >= h2s.length) {
-                idx = 0;
+            if (idx >= items.length) {
+                var nextPage = jQuery('.pages li>a:contains(' + (pageNum+1) + ')').attr('href');
+                if (nextPage) {
+                    url = nextPage;
+                } else {
+                    idx = 0;
+                }
             }
             doit = true;
         } else if (e.which == 107) { //prev
             idx--;
             if (idx < 0) {
-                idx = h2s.length;
+                var prevPage = jQuery('.pages li>a:contains(' + (pageNum-1) + ')').attr('href');
+                if (prevPage) {
+                    url = prevPage;
+                } else {
+                    idx = items.length;
+                }
             }
             doit = true;
         }
         if (doit) {
-            console.log('scrolling to ' + idx);
-            jQuery('html, body').animate({
-                scrollTop: jQuery(h2s[idx]).offset().top
-            }, 500);
+            if (url) {
+                console.log('going to ' + url);
+                document.location.href = url;
+            } else {
+                console.log('scrolling to ' + idx);
+                jQuery('html, body').animate({
+                    scrollTop: jQuery(items[idx]).offset().top
+                }, 500);
+            }
         }
     });
 })();
