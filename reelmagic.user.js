8148d624cdd4161e434936d6ba6dbcaa54b44c15
Alexander Q <qalexx@gmail.com>
2019-08-31 16:40:51 +0300

    supporting different places for name and playlist id

--- reelmagic.user.js
+++ reelmagic.user.js
@@ -8,8 +8,26 @@
 // ==/UserScript==
 
 function stuff() {
-    var playlist = jQuery('.wistia_embed').attr('id').replace(/^wistia_/, '');
-    var name = jQuery('[itemprop=name]').text().trim();
+    var playlist;
+    if (jQuery('script[src^="https://fast.wistia.com/embed/medias"]').length) {
+        console.log("playlist from script src");
+        playlist = jQuery('script[src^="https://fast.wistia.com/embed/medias"]').attr('src').match(/medias\/(\w+)/)[1]
+    } else if (jQuery('.wistia_embed').length) {
+        console.log("playlist from .wistia_embed");
+        playlist = jQuery('.wistia_embed').attr('id').replace(/^wistia_/, '')
+    } else if  (jQuery('iframe').length) {
+        console.log("playlist from iframt src");
+        playlist = jQuery('iframe').attr('src').match(/playlists\/(\w+)/)[1]
+    }
+
+    var name;
+    if (jQuery('.category-desc>h1:first').length)
+        name = jQuery('.category-desc>h1:first').text().trim()
+    else if (jQuery('[itemprop=name]').length)
+        name = jQuery('[itemprop=name]').text().trim()
+    else if (jQuery('.blog>h1:first').length)
+        name = jQuery('.blog>h1:first').text().trim();
+
     var str = "reelmagic_dl.ex -t '" + name + "' -p " + playlist;
     console.log(str);
     alert(str);
