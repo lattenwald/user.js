97afb4b579016f498265b3ec9f1b333504f6882e
Alexander Q <qalexx@gmail.com>
2021-10-02 21:58:27 +0300

    AndroidPolice new design

--- ap.navigation.user.js
+++ ap.navigation.user.js
@@ -1,44 +1,32 @@
 // ==UserScript==
 // @name         Apps list navigation for AndroidPolice
 // @namespace    q.alexander.androidpolice.navigation
-// @version      0.4.1
+// @version      0.5.0
 // @description  enter something useful
 // @author       You
 // @match        http://www.androidpolice.com/*
 // @match        https://www.androidpolice.com/*
 // @grant        none
+// @require      https://code.jquery.com/jquery-latest.min.js
 // ==/UserScript==
 
 //window.addEventListener('load', function() {
 (function(){
-    var h3s = jQuery('div.post-content h3');
+    var h3s = jQuery('section#article-body h3')
     var idx = -1;
-    var pageNum = Number(jQuery('.multi-page-post.pages>section>span').text().substring(5));
-    if (!pageNum)
-        pageNum=1;
     jQuery(document).keypress(function(e){
         var doit=false;
         var url=false;
-        if (e.which == 106) { //next
+        if (e.which == 106 || e.which == 1086) { //next
             idx++;
             if (idx >= h3s.length) {
-                var nextPage = jQuery('.multi-page-post.pages a>span:contains(Page '+(pageNum+1)+')').parent().attr('href');
-                if (nextPage) {
-                    url = nextPage;
-                } else {
-                    idx = 0;
-                }
+              idx = 0;
             }
             doit = true;
-        } else if (e.which == 107) { //prev
+        } else if (e.which == 107 || e.which == 1083) { //prev
             idx--;
             if (idx < 0) {
-                var prevPage = jQuery('.multi-page-post.pages a>span:contains(Page '+(pageNum-1)+')').parent().attr('href');
-                if (prevPage) {
-                    url = prevPage;
-                } else {
-                    idx = h3s.length;
-                }
+              idx = h3s.length;
             }
             doit = true;
         }
@@ -49,8 +37,8 @@
             } else {
                 console.log('scrolling to ' + idx);
                 jQuery('html, body').animate({
-	                scrollTop: jQuery(h3s[idx]).offset().top
-	            }, 500);
+                  scrollTop: jQuery(h3s[idx]).offset().top
+              }, 500);
             }
         }
     });
