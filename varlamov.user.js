17d4f65e8a87459cb49e335e10386b56cf48f4fd
Alexander Q <qalexx@gmail.com>
2021-11-16 13:53:13 +0300

    added varlamov.ru images navigation script

---
+++ varlamov.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Photos navigation for varlamov.ru
+// @namespace    q.alexander.varlamov.navigation
+// @version      0.1.0
+// @description  enter something useful
+// @author       You
+// @match        https://varlamov.ru/*
+// @grant        none
+// @require      https://code.jquery.com/jquery-latest.min.js
+// ==/UserScript==
+
+(function(){
+  var imgs = jQuery('article.article__content div.wrap')
+  var idx = -1;
+  console.log("found " + imgs.length + " images");
+  jQuery(document).keypress(function(e){
+    var doit=false;
+    var url=false;
+    if (e.which == 106 || e.which == 1086) { //next
+      idx++;
+      if (idx >= imgs.length) {
+        idx = 0;
+      }
+      doit = true;
+    } else if (e.which == 107 || e.which == 1083) { //prev
+      idx--;
+      if (idx < 0) {
+        idx = imgs.length;
+      }
+      doit = true;
+    }
+    if (doit) {
+      console.log('scrolling to ' + idx);
+      jQuery('html, body').animate({
+        scrollTop: jQuery(imgs[idx]).offset().top - 60
+      }, 500);
+    }
+  });
+})();
