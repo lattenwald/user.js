560e126a4a0cff9433f1114af68c0c7be592494c
Alexander Q <qalexx@gmail.com>
2021-11-20 23:12:22 +0300

    androidpolice

--- ap.navigation.user.js
+++ ap.navigation.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Apps list navigation for AndroidPolice
 // @namespace    q.alexander.androidpolice.navigation
-// @version      0.5.0
-// @description  enter something useful
+// @version      0.5.1
+// @description  something useful
 // @author       You
 // @match        http://www.androidpolice.com/*
 // @match        https://www.androidpolice.com/*
@@ -12,35 +12,29 @@
 
 //window.addEventListener('load', function() {
 (function(){
-    var h3s = jQuery('section#article-body h3')
-    var idx = -1;
-    jQuery(document).keypress(function(e){
-        var doit=false;
-        var url=false;
-        if (e.which == 106 || e.which == 1086) { //next
-            idx++;
-            if (idx >= h3s.length) {
-              idx = 0;
-            }
-            doit = true;
-        } else if (e.which == 107 || e.which == 1083) { //prev
-            idx--;
-            if (idx < 0) {
-              idx = h3s.length;
-            }
-            doit = true;
-        }
-        if (doit) {
-            if (url) {
-                console.log('going to ' + url);
-                document.location.href = url;
-            } else {
-                console.log('scrolling to ' + idx);
-                jQuery('html, body').animate({
-                  scrollTop: jQuery(h3s[idx]).offset().top
-              }, 500);
-            }
-        }
-    });
+  var items = jQuery('section#article-body h3,section#article-body h2')
+  var idx = -1;
+  jQuery(document).keypress(function(e){
+    var doit=false;
+    if (e.which == 106 || e.which == 1086) { //next
+      idx++;
+      if (idx >= items.length) {
+        idx = 0;
+      }
+      doit = true;
+    } else if (e.which == 107 || e.which == 1083) { //prev
+      idx--;
+      if (idx < 0) {
+        idx = items.length;
+      }
+      doit = true;
+    }
+    if (doit) {
+      console.log('scrolling to ' + idx);
+      jQuery('html, body').animate({
+        scrollTop: jQuery(items[idx]).offset().top
+      }, 500);
+    }
+  });
 })();
 //}, false);
