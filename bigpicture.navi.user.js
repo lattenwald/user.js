027e6ea0f2fb16aee2bcb623e687e0b07f8d41fe
Alexander Q <qalexx@gmail.com>
2022-10-12 13:37:13 +0300

    androidauthority

--- bigpicture.navi.user.js
+++ bigpicture.navi.user.js
@@ -5,36 +5,37 @@
 // @include     https://www.bostonglobe.com/news/bigpicture/*
 // @version     2.1
 // @grant       none
+// @require https://code.jquery.com/jquery-3.2.1.min.js
 // ==/UserScript==
 
 (function(){
-    var set_body_height = function () { $('body').css('height', $(window).height()); };
+    var set_body_height = function () { jQuery('body').css('height', jQuery(window).height()); };
 
-    $('head').append('<style type="text/css">article.pcaption {opacity: 0} article.pcaption:hover {opacity: 0.9}</style>');
+    jQuery('head').append('<style type="text/css">article.pcaption {opacity: 0} article.pcaption:hover {opacity: 0.9}</style>');
 
-    $('div#contain').css('height', '100%');
-    $('div#container').css('height', '100%');
-    $('div#container>section').css('height', '100%');
-    $('img').css('max-height', '100%');
+    jQuery('div#contain').css('height', '100%');
+    jQuery('div#container').css('height', '100%');
+    jQuery('div#container>section').css('height', '100%');
+    jQuery('img').css('max-height', '100%');
 
-    $('div.mfp-wrap').remove();
-    $('div.mfp-bg').click();
-    $('header.bg-header').remove();
-    $('div#container').nextAll().remove();
+    jQuery('div.mfp-wrap').remove();
+    jQuery('div.mfp-bg').click();
+    jQuery('header.bg-header').remove();
+    jQuery('div#container').nextAll().remove();
 
-    $('article.pcaption').each(function(){
-        $(this).appendTo($(this).prev());
-        $(this).css('position', 'relative');
-        $(this).css('bottom', '100%');
-        $(this).css('background-color', '#000000');
+    jQuery('article.pcaption').each(function(){
+        jQuery(this).appendTo(jQuery(this).prev());
+        jQuery(this).css('position', 'relative');
+        jQuery(this).css('bottom', '100%');
+        jQuery(this).css('background-color', '#000000');
     });
-    $('div.photo').css('height', '100%');
+    jQuery('div.photo').css('height', '100%');
 
-    $(window).bind('resize', set_body_height);
+    jQuery(window).bind('resize', set_body_height);
     set_body_height();
 
     // navigation
-    var imgs = $('div.photo');
+    var imgs = jQuery('div.photo');
     var idx = -1;
     jQuery(document).keypress(function(e){
         var doit=false;
@@ -50,7 +51,7 @@
         if (doit) {
             console.log('scrolling to ' + idx);
             jQuery('html, body').animate({
-                scrollTop: $(imgs[idx]).offset().top
+                scrollTop: jQuery(imgs[idx]).offset().top
             }, 500);
         }
     });
